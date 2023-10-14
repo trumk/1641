4dd6b2e8be880d2ac81d9dafd8fcb3bc298cfff6
trumk <nguyentrungtest2292003@gmail.com>
2023-10-14 08:36:21 +0700

    if statement

--- 14-10-2023.py
+++ 14-10-2023.py
@@ -1,3 +1,9 @@
+if 5 > 2:
+  print("Five is greater than two!") #worked
+
+if 5 > 2:
+print("Five is greater than two!") # not worked
+
 x = 5
 y = "Hello world"
 print("This is x:", x, ", This is y:", y)
