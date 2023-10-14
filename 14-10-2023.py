b330163cd95729d00af94411b8d9a19cecf4be9a
trumk <nguyentrungtest2292003@gmail.com>
2023-10-14 08:42:50 +0700

    variable continue

--- 14-10-2023.py
+++ 14-10-2023.py
@@ -1,9 +1,16 @@
 if 5 > 2:
   print("Five is greater than two!") #worked
 
-if 5 > 2:
-print("Five is greater than two!") # not worked
+#if 5 > 2:
+#print("Five is greater than two!") # not worked
 
-x = 5
-y = "Hello world"
-print("This is x:", x, ", This is y:", y)
+o = 5
+i = "Hello world"
+print("This is x:", o, ", This is y:", i)
+
+#Variables do not need to be declared with any particular type, and can even change type after they have been set.
+x = 4
+x = "Sally"
+print(x)
+
+
