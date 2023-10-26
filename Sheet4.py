003097c4a627a0e9e058130a891ee5dd58aa3ee0
trumk <nguyentrungtest2292003@gmail.com>
2023-10-26 15:42:06 +0700

    sheet4

---
+++ Sheet4.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+csv_file_path = r'C:\Users\loan\OneDrive\Desktop\filtereddata.csv'
+
+df = pd.read_csv(csv_file_path)
+
+df = df.sort_values(by='owners', ascending=False)
+
+df = df.sort_values(by='positive_ratings', ascending=False)
+
+top_8_games = df.head(8)
+
+print(top_8_games)
