010ebf0825084bc0badae81f01736c6fd173c049
trumk <nguyentrungtest2292003@gmail.com>
2023-10-26 15:12:52 +0700

    Sheet 2 & 3

---
+++ Sheet2.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+csv_file_path = r'C:\Users\loan\OneDrive\Desktop\filtereddata.csv'
+
+
+df = pd.read_csv(csv_file_path)
+
+
+df = df.sort_values(by='achievements', ascending=False)
+
+
+top_8_achievements = df.head(8)
+
+
+print(top_8_achievements)
---
+++ Sheet3.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+csv_file_path = r'C:\Users\loan\OneDrive\Desktop\filtereddata.csv'
+
+df = pd.read_csv(csv_file_path)
+
+filtered_df = df[(df['price'] >= 10) & (df['price'] <= 20)]
+
+filtered_df = filtered_df.sort_values(by='average_playtime', ascending=False)
+
+top_5_games = filtered_df.head(5)
+
+print(top_5_games)
