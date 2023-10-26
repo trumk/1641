9a8c3fbf0b4fb0e85f240dbc943e0ab0d0892c79
trumk <nguyentrungtest2292003@gmail.com>
2023-10-26 13:14:15 +0700

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,12 +1,9 @@
 import pandas as pd
 
-# Đường dẫn đến tệp CSV gốc
 csv_file_path = r'C:\Users\loan\OneDrive\Desktop\steam.csv'
 
-# Đọc tệp CSV vào một DataFrame
 df = pd.read_csv(csv_file_path)
 
-# Lọc dữ liệu theo các điều kiện
 filtered_df = df[
     (df["achievements"] != 0) &
     (df["positive_ratings"] != 0) &
@@ -16,9 +13,7 @@ filtered_df = df[
     (df["price"] != 0)
 ]
 
-# Chọn ra 700 dòng dữ liệu
 filtered_df = filtered_df.head(700)
 
-# Lưu kết quả vào một tệp CSV mới
 filtered_csv_path = r'C:\Users\loan\OneDrive\Desktop\filtereddata.csv'
 filtered_df.to_csv(filtered_csv_path, index=False)
