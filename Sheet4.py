2090fe4d424bf0a01f804cbf576182d628b83bbd
trumk <nguyentrungtest2292003@gmail.com>
2023-10-27 16:13:43 +0700

    Update Sheet4.py

--- Sheet4.py
+++ Sheet4.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-# Đọc dữ liệu từ tệp CSV
+
 df = pd.read_csv(r'C:\Users\loan\OneDrive\Desktop\filtereddata.csv')
 
 df['release_date'] = pd.to_datetime(df['release_date'], errors='coerce')
@@ -9,6 +9,7 @@ games_in_2013 = df[df['release_date'].dt.year == 2013]
 
 games_in_2013 = games_in_2013.sort_values(by='release_date')
 
-random_games_2013 = games_in_2013.head(15)
+random_games_2013 = games_in_2013.sample(15, random_state=1) 
 
 print(random_games_2013)
+    
