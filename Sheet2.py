0655a9c976ec9d9e70779f9ef382da4f83d8f6d5
trumk <nguyentrungtest2292003@gmail.com>
2023-10-27 09:00:40 +0700

    Update Sheet2.py

--- Sheet2.py
+++ Sheet2.py
@@ -8,4 +8,6 @@ df['release_year'] = pd.to_datetime(df['release_date'], errors='coerce').dt.year
 
 games_2015 = df[df['release_year'] == 2015]
 
-print(games_2015)
+top_8_achievements = games_2015.sort_values(by='achievements', ascending=False).head(8)
+
+print(top_8_achievements)
