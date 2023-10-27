a362316f7f6b9346d79eec6836ba464d14f850d1
trumk <nguyentrungtest2292003@gmail.com>
2023-10-27 08:54:20 +0700

    Update Sheet2.py

--- Sheet2.py
+++ Sheet2.py
@@ -2,14 +2,10 @@ import pandas as pd
 
 csv_file_path = r'C:\Users\loan\OneDrive\Desktop\filtereddata.csv'
 
-
 df = pd.read_csv(csv_file_path, encoding='latin-1')
 
+df['release_year'] = pd.to_datetime(df['release_date'], errors='coerce').dt.year
 
-df = df.sort_values(by='achievements', ascending=False)
+games_2015 = df[df['release_year'] == 2015]
 
-
-top_8_achievements = df.head(8)
-
-
-print(top_8_achievements)
+print(games_2015)
