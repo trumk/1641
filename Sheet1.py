6f9940bc0c5e0c5f0ea371d5c95f589b8a4d0214
trumk <nguyentrungtest2292003@gmail.com>
2023-10-29 10:05:27 +0700

    Update Sheet1.py

--- Sheet1.py
+++ Sheet1.py
@@ -2,14 +2,10 @@ import pandas as pd
 
 csv_file_path = r'C:\Users\loan\OneDrive\Desktop\filtereddata.csv'
 
-
 df = pd.read_csv(csv_file_path, encoding='latin-1')
-
 
 df = df.sort_values(by='average_playtime', ascending=False)
 
-
 top_8_games = df.head(8)
-
 
 print(top_8_games)
