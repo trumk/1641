4f43741f6ec231512ecc77f72db6c3c96d73c63e
trumk <nguyentrungtest2292003@gmail.com>
2023-10-29 08:42:54 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,6 @@ csv_file_path = r'C:\Users\loan\OneDrive\Desktop\filtereddata.csv'
 
 df = pd.read_csv(csv_file_path, encoding='latin-1')
 
-countries_in_southeast_asia = ["Vietnam", "Thailand", "Indonesia", "Malaysia", "Singapore", "Philippines", "Myanmar", "Cambodia", "Laos", "Brunei", "Timor-Leste"]
+df['price'] = np.random.randint(500, 1201, size=len(df))
 
-def generate_region():
-    region = np.random.choice(countries_in_southeast_asia, max(5, np.random.randint(1, 4)), replace=False)
-    return ", ".join(region)
-
-df['region'] = df.apply(lambda row: generate_region(), axis=1)
-
-df.to_csv(r'C:\Users\loan\OneDrive\Desktop\filtereddata.csv', index=False)
+df.to_csv(csv_file_path, index=False)
