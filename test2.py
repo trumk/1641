0ab4ada80963e89d86d6ec0bf687dcec157a38f4
trumk <nguyentrungtest2292003@gmail.com>
2023-10-31 16:41:17 +0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -5,10 +5,9 @@ csv_file_path = r'C:\Users\loan\OneDrive\Desktop\filtereddata.csv'
 
 df = pd.read_csv(csv_file_path, encoding='latin-1')
 
-#them cot region
-# countries_in_southeast_asia = ["Vietnam", "Thailand", "Indonesia", "Malaysia", "Singapore", "Philippines", "Myanmar", "Cambodia", "Laos", "Brunei", "Timor-Leste"]
+countries_in_southeast_asia = ["Vietnam", "Thailand", "Indonesia", "Malaysia", "Singapore", "Philippines", "Myanmar", "Cambodia", "Laos", "Brunei", "Timor-Leste"]
 
-# df['region'] = [" ".join(np.random.choice(countries_in_southeast_asia, np.random.randint(1, 4))) for _ in range(len(df))]
+df['region'] = [" ".join(np.random.choice(countries_in_southeast_asia, np.random.randint(1, 4))) for _ in range(len(df))]
 
 df = df.drop('region', axis=1) #xoa cot region
 
